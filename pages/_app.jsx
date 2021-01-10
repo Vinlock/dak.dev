85d8f2f7279204cde74c98ac91fe4fa44a43977a
Dak Washbrook <vinlockz@gmail.com>
2021-01-10 14:26:50 -0800

    Update icons

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -19,12 +19,28 @@ library.add(
   faCaretCircleDown,
 )
 
+const title = 'Dak Washbrook | Software Engineer'
+const description = 'Self-motivated Software Engineer with a passion for understanding and always learning.'
+const keywords = 'software engineer, software developer, computer science, programming, golang, javascript, nodejs'
+
 const App = ({ Component, pageProps }) => {
   return (
     <AppProvider>
       <div className="font-body">
         <Head>
-          <title>dak.dev</title>
+          <title>{title}</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+          <meta name="description" content={description} />
+          <meta name="keywords" content={keywords} />
+          <meta name="og:url" property="og:url" content="https://dak.dev" />
+          <meta name="og:title" property="og:title" content={title} />
+          <meta name="og:type" property="og:type" content="website" />
+          <meta name="og:description" property="og:description" content={description} />
+          <meta name="theme-color" content="#101010" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link rel="preconnect" href="https://use.typekit.net" />
           <link href="https://fonts.googleapis.com/css2?family=Sen:wght@400;700;800&display=block" rel="stylesheet" />
